af7307d0244bea84f46fe10d29f086b20da74eb5
Jérôme Renard <jeromer@fastmail.net>
2019-01-25 15:36:27 +0100

    Avoid downloading riemann all the time
    Riemann is downloaded everytime make integ is run.
    
    This commit avoids this download by checking if riemann is already
    present and downloads it only when not found.

--- integration.sh
+++ integration.sh
@@ -5,12 +5,17 @@ set -u
 RIEMANN_VERSION=$1
 RIEMANN_URL=https://github.com/riemann/riemann/releases/download/${RIEMANN_VERSION}/riemann-${RIEMANN_VERSION}.tar.bz2
 
-echo "Download Riemann"
-wget "${RIEMANN_URL}"
-echo "Untar Riemann"
-tar xjf "riemann-${RIEMANN_VERSION}.tar.bz2"
+BIN=riemann-${RIEMANN_VERSION}/bin/riemann
+
+if [ ! -f "${BIN}" ]; then
+    echo "Download Riemann"
+    wget "${RIEMANN_URL}"
+    echo "Untar Riemann"
+    tar xjf "riemann-${RIEMANN_VERSION}.tar.bz2"
+fi
+
 echo "Launch Riemann"
-"riemann-${RIEMANN_VERSION}/bin/riemann" &
+${BIN} &
 RIEMANN_PID=$!
 sleep 10
 echo "Launch tests"
