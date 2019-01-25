e26e936df22950cb4683366db70f82efb84d8371
Jérôme Renard <jeromer@fastmail.net>
2019-01-25 15:17:25 +0100

    Fix shellcheck errors
    In ./integration.sh line 9:
    wget ${RIEMANN_URL}
         ^-- SC2086: Double quote to prevent globbing and word splitting.
    
    In ./integration.sh line 11:
    tar xjf riemann-${RIEMANN_VERSION}.tar.bz2
                    ^-- SC2086: Double quote to prevent globbing and word splitting.
    
    In ./integration.sh line 13:
    riemann-${RIEMANN_VERSION}/bin/riemann &
            ^-- SC2086: Double quote to prevent globbing and word splitting.

--- integration.sh
+++ integration.sh
@@ -6,11 +6,11 @@ RIEMANN_VERSION=$1
 RIEMANN_URL=https://github.com/riemann/riemann/releases/download/${RIEMANN_VERSION}/riemann-${RIEMANN_VERSION}.tar.bz2
 
 echo "Download Riemann"
-wget ${RIEMANN_URL}
+wget "${RIEMANN_URL}"
 echo "Untar Riemann"
-tar xjf riemann-${RIEMANN_VERSION}.tar.bz2
+tar xjf "riemann-${RIEMANN_VERSION}.tar.bz2"
 echo "Launch Riemann"
-riemann-${RIEMANN_VERSION}/bin/riemann &
+"riemann-${RIEMANN_VERSION}/bin/riemann" &
 RIEMANN_PID=$!
 sleep 10
 echo "Launch tests"
